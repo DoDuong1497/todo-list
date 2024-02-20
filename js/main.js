7bbfab01c216a0b726aa89a7f72044ae3858df03
DoDuong1497 <duongdo1497@gmail.com>
2024-02-20 23:16:47 +0700

    update localStorage

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-let dataTask = [
-  {
-    id: Math.round(Math.random() * 100),
-    title: 'Lorem ipsum dolor sit amet',
-    date: new Date().toLocaleDateString(),
-  },
-  {
-    id: Math.round(Math.random() * 100),
-    title: 'Lorem ipsum dolor sit amet 3',
-    date: new Date().toLocaleDateString(),
-  },
-  {
-    id: Math.round(Math.random() * 100),
-    title: 'Lorem ipsum dolor sit amet 2',
-    date: new Date().toLocaleDateString(),
-  },
-];
+// let dataTask = [
+//   {
+//     id: Math.round(Math.random() * 100),
+//     title: 'Lorem ipsum dolor sit amet',
+//     date: new Date().toLocaleDateString(),
+//   },
+//   {
+//     id: Math.round(Math.random() * 100),
+//     title: 'Lorem ipsum dolor sit amet 3',
+//     date: new Date().toLocaleDateString(),
+//   },
+//   {
+//     id: Math.round(Math.random() * 100),
+//     title: 'Lorem ipsum dolor sit amet 2',
+//     date: new Date().toLocaleDateString(),
+//   },
+// ];
 
+let dataLocal = JSON.parse(localStorage.getItem('data'));
 let toDoList = document.querySelector('.todo-list ul');
 
 function renderListTask(data) {
@@ -41,7 +42,7 @@ function renderListTask(data) {
   });
 }
 
-renderListTask(dataTask);
+renderListTask(dataLocal);
 
 let formTask = document.querySelector('.todo-add form');
 
@@ -56,27 +57,38 @@ formTask.addEventListener('submit', (e) => {
     date: new Date().toLocaleDateString(),
   };
 
-  let newDataTask = [...dataTask, newDataTaskItem];
+  let newDataTask = [...dataLocal, newDataTaskItem];
+  dataLocal.push(newDataTaskItem);
 
-  dataTask.push(newDataTaskItem);
+  localStorage.setItem('data', JSON.stringify(dataLocal));
 
   renderListTask(newDataTask);
+  document.getElementById('taskName').value = '';
 });
 
 function deleteTaskItem(taskId) {
-  let taskItemIndex = dataTask.findIndex((ele) => ele.id === taskId);
+  let taskItemIndex = dataLocal.findIndex((ele) => ele.id === taskId);
   if (taskItemIndex !== -1) {
-    dataTask.splice(taskItemIndex, 1);
+    dataLocal.splice(taskItemIndex, 1);
   }
 
-  renderListTask(dataTask);
+  localStorage.removeItem('data');
+
+  renderListTask(dataLocal);
 }
 
-let checkbox = document.querySelectorAll('.todo-list__checkbox');
+// let checkbox = document.querySelectorAll('.todo-list__checkbox');
 
-checkbox.forEach((ele) => {
-  ele.addEventListener('click', (e) => {
-    let taskItem = e.target.parentNode.parentElement;
+// checkbox.forEach((ele) => {
+//   ele.addEventListener('click', (e) => {
+//     let taskItem = e.target.parentNode.parentElement;
+//     taskItem.classList.toggle('checked');
+//   });
+// });
+
+toDoList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('todo-list__checkbox')) {
+    let taskItem = e.target.closest('li');
     taskItem.classList.toggle('checked');
-  });
+  }
 });
